4406a9c3732984f2efe34a0fba84613569ef08eb
Edynilson Silva <edynilson.antonio@gmail.com>
2020-09-17 19:42:45 -0300

    Utilização do fetch_assoc e data_seek

--- index.php
+++ index.php
@@ -3,4 +3,10 @@
 $conexao = new mysqli('127.0.0.1', 'root', '', 'treinaweb'); // MYSQLI = Orientada a Objetos (classes)
 
 $sql = "SELECT * FROM alunos";
-var_dump($conexao->query($sql));
+$resultado = $conexao->query($sql);
+
+// Posso especificar o ponteiro (neste exemplo, pegaria o 3° registro da tabela)
+$resultado->data_seek(2);
+
+// Retorna uma linha por vez
+var_dump($resultado->fetch_assoc());
