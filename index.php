566d7463b46894b104500321700e8295a91f6c8a
Edynilson Silva <edynilson.antonio@gmail.com>
2020-09-17 19:30:08 -0300

    Inserindo registro na tabela

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 $conexao = new mysqli('127.0.0.1', 'root', '', 'treinaweb'); // MYSQLI = Orientada a Objetos (classes)
 
-$sql = 'CREATE TABLE alunos (id int primary key, nome varchar(100))';
+$sql = "INSERT INTO alunos (id, nome) VALUES (1, 'João da Silva')";
 var_dump($conexao->query($sql));
