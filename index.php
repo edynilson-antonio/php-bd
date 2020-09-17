55430dc7d58cbe070ea205778f53ea9aed45ff57
Edynilson Silva <edynilson.antonio@gmail.com>
2020-09-17 19:33:35 -0300

    Select na tabela alunos e retorno da instância da classe mysqli_result

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 $conexao = new mysqli('127.0.0.1', 'root', '', 'treinaweb'); // MYSQLI = Orientada a Objetos (classes)
 
-$sql = "INSERT INTO alunos (id, nome) VALUES (1, 'João da Silva')";
+$sql = "SELECT * FROM alunos";
 var_dump($conexao->query($sql));
